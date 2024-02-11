b3adad9bbd4688de675b9bce5967ef0058c0a725
Ali Amin <aliamin745@gmail.com>
2024-02-11 16:47:41 -0500

    Implemented handlers and endpoints for Shortening urls, tested with POSTMAN

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"github.com/Ali7109/go-url-shortener/handler"
+	"github.com/Ali7109/go-url-shortener/store"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,9 +12,18 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hey Go URL Shortener",
+			"message": "Welcome to the URL Shortener API",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect((c))
+	})
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
