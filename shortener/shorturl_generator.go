bb089bff49ca8ece98fcd61210392880ddc76ebd
Ali Amin <aliamin745@gmail.com>
2024-02-11 13:23:21 -0500

    Url hashing and generating functions complete

---
+++ shortener/shorturl_generator.go
@@ -0,0 +1,43 @@
+package shortener
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/itchyny/base58-go"
+)
+
+// ShortURLGenerator is a type that generates short URLs
+
+func sha256Of(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write([]byte(input))
+	return algorithm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
+}
+
+/*
+*
+Procedure:
+1. Generate a SHA-256 hash of the input URL
+2. Convert the hash to a base58 string
+3. Take the first 8 characters of the base64 string
+4. Return the result
+*/
+func GenerateShortLink(intialLink string, userId string) string {
+	urlHashBytes := sha256Of(intialLink + userId)
+	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
+	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
+	return finalString[:8]
+}
